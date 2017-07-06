93629a17578014db5aa399453cf68720c1b52708
pylipp <philipp.metzner@tum.de>
2017-07-06 21:01:00 +0200

    Arduino code cleanup.
    - tidy whitespace and indents
    - remove unnecessary variables

--- src/ros_temp_laser.ino
+++ src/ros_temp_laser.ino
@@ -11,57 +11,46 @@
  * - temperature module at A0
  */
 
-int  ledPin  =  9;   // select the pin for  the LED
+int ledPin = 9;   // select the pin for  the LED
+int sensorPin = A0;    // select the input  pin for  the potentiometer 
 
 ros::NodeHandle nh;
 bool laserOn = false;
 
-void switch_laser_cb(const std_msgs::Empty& msg) {
-  digitalWrite(ledPin, (laserOn) ? LOW : HIGH);
-  laserOn = !laserOn;
-  delay(50);
+void switch_laser_cb(const std_msgs::Empty& msg) 
+{
+    digitalWrite(ledPin, (laserOn) ? LOW : HIGH);
+    laserOn = !laserOn;
+    delay(50);
 }
 ros::Subscriber<std_msgs::Empty> sub_switch_laser("switch_laser", switch_laser_cb);
 
 std_msgs::Int16 msg_temperature;
 ros::Publisher pub_temperature("temperature", &msg_temperature);
 
-int  sensorPin  =  A0;     // select the input  pin for  the potentiometer 
-int  sensorValue =  0;  // variable to  store  the value  coming  from  the sensor
+// counter variable to publish at lower frequency
 int counter = 0;
 
 void publishTemperature() 
 {
-  if (counter % 50 == 0) {
-  sensorValue = analogRead(sensorPin);
-  msg_temperature.data = sensorValue;
-  pub_temperature.publish(&msg_temperature);
-  }
-  counter++;
+    if (counter % 50 == 0) {
+        msg_temperature.data = analogRead(sensorPin);
+        pub_temperature.publish(&msg_temperature);
+    }
+    counter++;
 }
 
 void setup()
 {
-  nh.initNode();
-  nh.subscribe(sub_switch_laser);
-  nh.advertise(pub_temperature);
-  pinMode(ledPin, OUTPUT);
-  digitalWrite(ledPin, LOW);
+    nh.initNode();
+    nh.subscribe(sub_switch_laser);
+    nh.advertise(pub_temperature);
+    pinMode(ledPin, OUTPUT);
+    digitalWrite(ledPin, LOW);
 }
 
 void loop(){
-  nh.spinOnce();
-  publishTemperature();
-  delay(10);
+    nh.spinOnce();
+    publishTemperature();
+    delay(10);
 }
-/* 
-sensorValue =  analogRead(sensorPin);
-if (sensorValue > threshold) {
-digitalWrite(ledPin,  HIGH); 
-delay(50); 
-digitalWrite(ledPin,  LOW);
-delay(50);
-}
-Serial.println(sensorValue,  DEC);
-}*/
-
